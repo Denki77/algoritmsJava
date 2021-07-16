07e16b0793f100bd43623d91c062e731afd828c1
Denis_Fomenkov <tgimrv@ya.ru>
2021-07-16 18:29:57 +0300

    finish hw6 Количество деревьев 20000 из них сбалансированно 785

---
+++ src/lesson6/MyTreeMap.java
@@ -0,0 +1,219 @@
+package lesson6;
+
+import java.util.NoSuchElementException;
+
+public class MyTreeMap<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        K key;
+        V value;
+        Node left;
+        Node right;
+        int size;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            size = 1;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.size;
+    }
+
+    public int deep() {
+        return deep(root);
+    }
+
+    private int deep(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(deep(node.left), deep(node.right));
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    private void checkKeyNotNull(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("key не должен быть null");
+        }
+    }
+
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    public V get(K key) {
+        checkKeyNotNull(key);
+        return get(root, key);
+    }
+
+    private V get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) {
+            return node.value;
+        } else if (cmp < 0) {
+            return get(node.left, key);
+        } else {
+            return get(node.right, key);
+        }
+    }
+
+    public void put(K key, V value) {
+        checkKeyNotNull(key);
+        if (value == null) {
+            //delete(key);
+            return;
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp == 0) {
+            node.value = value;
+        } else if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else {
+            node.right = put(node.right, key, value);
+        }
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    public K minKey() {
+        return min(root).key;
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return min(node.left);
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteMin(node.left);
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    public K maxKey() {
+        return max(root).key;
+    }
+
+    private Node max(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return max(node.right);
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node node) {
+        if (node.right == null) {
+            return node.left;
+        }
+        node.right = deleteMax(node.right);
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    public boolean isBalance() {
+        if (isEmpty()) {
+            return true;
+        }
+        return isBalance(root);
+    }
+
+    private boolean isBalance(Node node) {
+        if (node.right == null) {
+            if (node.left != null) {
+                return node.left.size < 2;
+            }
+            return true;
+        } else if (node.left == null) {
+            return node.right.size < 2;
+        }
+
+        return isBalance(node.right) && isBalance(node.left);
+    }
+
+    public void delete(K key) {
+        checkKeyNotNull(key);
+        root = delete(root, key);
+    }
+
+    private Node delete(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
+        } else if (cmp > 0) {
+            node.right = delete(node.right, key);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            Node temp = node;
+            node = min(node.right);
+            node.right = deleteMin(temp.right);
+            node.left = temp.left;
+        }
+        node.size = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    @Override
+    public String toString() {
+        return toString(root);
+    }
+
+    private String toString(Node node) {
+        if (node == null) {
+            return "";
+        }
+        return toString(node.left) + " " +
+                node.key + " = " + node.value + " " +
+                toString(node.right);
+    }
+}
