05253e3796f0e4e6d3d36205c8c81be534e62589
Denis_Fomenkov <tgimrv@ya.ru>
2021-07-17 11:27:14 +0300

    finish hw7 изображение графа в graph.png вынес одинаковый код в абстрактный класс

---
+++ src/lesson7/BreadthFirstPath.java
@@ -0,0 +1,29 @@
+package lesson7;
+
+import java.util.LinkedList;
+
+public class BreadthFirstPath extends AbstractFirstPath {
+
+    public BreadthFirstPath(Graph g, int source) {
+        super(g, source);
+        bfs(g, source);
+    }
+
+    private void bfs(Graph g, int source) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        marked[source] = true;
+
+        while (!queue.isEmpty()) {
+            int vertex = queue.removeFirst();
+            for (int w : g.getAdjList(vertex)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = vertex;
+                    queue.addLast(w);
+                }
+            }
+        }
+    }
+
+}
